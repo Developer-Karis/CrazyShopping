0c5b8f38017db2c2c675ce3906f7eea7401f414d
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-07-19 15:58:28 +0200

    avancement navbar, update 15:58

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function buttonLogIn() {
+    let inputUsername = document.getElementById("username");
+    let inputPassword = document.getElementById("password");
+    let messageEmpty = document.getElementById("empty");
+
+    if (inputUsername.value.length == 0 && inputPassword.value.length == 0) {
+        messageEmpty.innerHTML = "You must enter Username and Password First !";
+        messageEmpty.style.color = "red";
+    }
+}
